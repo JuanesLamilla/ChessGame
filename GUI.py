dc95c86d1e5974d8d71748eb1252745549d1ae8c
adrian-1219 <adrian-1219@users.noreply.github.com>
2019-11-13 06:40:34 -0500

    Modified GUI to work with updated board class
    GUI now assigns an image depending on the piece subclass type and its colour.

--- GUI.py
+++ GUI.py
@@ -5,6 +5,7 @@ def draw_board(screen: pygame.Surface, width: int, animation:bool =False):
     """
     Draws the chess board on the given screen, according to the given width.
     """
+    screen.fill(pygame.Color(255,255,255))
     y = 0
     grid_width = width / 8
     for row in range (8):
@@ -30,34 +31,39 @@ def draw_pieces(screen: pygame.Surface, width: int, board: Board,
     grid_width = width / 8
     for row in range(8):
         for col in range(8):
-            if board.board[row][col]:
+            if board.board[row][col] != 0:
                 x = grid_width * col
                 y = grid_width * row
-                if board.board[row][col] == 'B_R':
-                    piece_image = pygame.image.load("PiecesPNG/BRook.png")
-                elif board.board[row][col] == 'W_R':
-                    piece_image = pygame.image.load("PiecesPNG/WRook.png")
-                elif board.board[row][col] == 'B_Kn':
-                    piece_image = pygame.image.load("PiecesPNG/BKnight.png")
-                elif board.board[row][col] == 'W_Kn':
-                    piece_image = pygame.image.load("PiecesPNG/WKnight.png")
-                elif board.board[row][col] == 'B_B':
-                    piece_image = pygame.image.load("PiecesPNG/BBishop.png")
-                elif board.board[row][col] == 'W_B':
-                    piece_image = pygame.image.load("PiecesPNG/WBishop.png")
-                elif board.board[row][col] == 'B_Q':
-                    piece_image = pygame.image.load("PiecesPNG/BQueen.png")
-                elif board.board[row][col] == 'W_Q':
-                    piece_image = pygame.image.load("PiecesPNG/WQueen.png")
-                elif board.board[row][col] == 'B_K':
-                    piece_image = pygame.image.load("PiecesPNG/BKing.png")
-                elif board.board[row][col] == 'W_K':
-                    piece_image = pygame.image.load("PiecesPNG/WKing.png")
-                elif board.board[row][col] == 'B_P':
-                    piece_image = pygame.image.load("PiecesPNG/BPawn.png")
-                elif board.board[row][col] == 'W_P':
-                    piece_image = pygame.image.load("PiecesPNG/WPawn.png")
-                    
+                if board.board[row][col].__class__.__name__ == 'Rook':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BRook.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WRook.png")
+                elif board.board[row][col].__class__.__name__ == 'Knight':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BKnight.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WKnight.png")
+                elif board.board[row][col].__class__.__name__ == 'Bishop':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BBishop.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WBishop.png")
+                elif board.board[row][col].__class__.__name__ == 'Queen':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BQueen.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WQueen.png")
+                elif board.board[row][col].__class__.__name__ == 'King':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BKing.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WKing.png")
+                elif board.board[row][col].__class__.__name__ == 'Pawn':
+                    if board.board[row][col].colour == 'B':
+                        piece_image = pygame.image.load("PiecesPNG/BPawn.png")
+                    else:
+                        piece_image = pygame.image.load("PiecesPNG/WPawn.png")
                 screen.blit(piece_image, (x-5, y-5))
             if animation and row == 3:
                 pygame.time.delay(50)
@@ -72,7 +78,6 @@ def main():
     board = Board()
     animation = True
     screen = pygame.display.set_mode((width,width))
-    screen.fill(pygame.Color(255,255,255))
     draw_board(screen, width, animation)
     draw_pieces(screen, width, board, animation)
     pygame.display.update()
@@ -88,7 +93,6 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                screen.fill(pygame.Color(255,255,255))
                 x = int(event.pos[0] // grid_width)
                 y = int(event.pos[1] // grid_width)
                 draw_board(screen, width, False)
