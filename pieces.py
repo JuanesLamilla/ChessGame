740e367baafc00f08867d58c78cc191be217f5d7
Juan Lamilla <juan.lamilla@mail.utoronto.ca>
2019-12-19 10:18:12 -0500

    Remove redundant code in pieces.py
    Check_valid_move functionality covered in board.py, no longer needed in pieces.py

--- pieces.py
+++ pieces.py
@@ -1,6 +1,3 @@
-from math import sqrt
-
-
 class Piece:
     """A chess piece located on the chess board.
 
@@ -14,15 +11,6 @@ class Piece:
         """Initialize a new Piece with a given colour.
         """
         self.colour = colour
-
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Returns true iff the attempted move for the piece is valid.
-        For both current_location and attempted_location,
-        .._location[0] is the x coordinate and
-        .._location[1] is the y coordinate.
-        """
-        raise NotImplementedError
 
 
 class Pawn(Piece):
@@ -44,21 +32,6 @@ class Pawn(Piece):
         Piece.__init__(self, colour)
         self._start = True
 
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """If the pawn has yet to move, return true if the attempted_location
-        is one or two spaces forward. If the pawn has already moved, return
-        true if the attempted_location is only one space forward.
-        Return false otherwise."""
-
-        if current_location[0] == attempted_location[0]:
-            if self._start and\
-                        current_location[1] == (attempted_location[1] - 2):
-                    return True
-            elif current_location[1] == (attempted_location[1] - 1):
-                return True
-        return False
-
     def made_first_move(self) -> None:
         self._start = False
 
@@ -76,37 +49,6 @@ class Knight(Piece):
     def __init__(self, colour: str) -> None:
         Piece.__init__(self, colour)
 
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Return true iff the attempted_location is two spaces in any
-        direction and the one space to either side. Return false otherwise."""
-
-        # Checks above and to both sides
-        if attempted_location[1] == current_location[1] + 2 and \
-                (attempted_location[0] == current_location[0] + 1 or
-                    attempted_location[0] == current_location[0] - 1):
-                return True
-
-        # Checks below and to both sides
-        elif attempted_location[1] == current_location[1] - 2 and \
-                (attempted_location[0] == current_location[0] + 1 or
-                    attempted_location[0] == current_location[0] - 1):
-                return True
-
-        # Checks to the right and to up or down
-        elif attempted_location[0] == current_location[0] + 2 and \
-                (attempted_location[1] == current_location[1] + 1 or
-                    attempted_location[1] == current_location[1] - 1):
-                return True
-
-        # Checks to the left and to up or down
-        elif attempted_location[0] == current_location[0] - 2 and \
-                (attempted_location[1] == current_location[1] + 1 or
-                    attempted_location[1] == current_location[1] - 1):
-                return True
-
-        return False
-
 
 class Rook(Piece):
     """A chess rook located on the chess board.
@@ -120,16 +62,6 @@ class Rook(Piece):
 
     def __init__(self, colour: str) -> None:
         Piece.__init__(self, colour)
-
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Return true iff attempted_location is on the same x or y plane
-        as current_location."""
-
-        if current_location[0] == attempted_location[0] or \
-                current_location[1] == attempted_location[1]:
-            return True
-        return False
 
 
 class Bishop(Piece):
@@ -145,18 +77,6 @@ class Bishop(Piece):
     def __init__(self, colour: str) -> None:
         Piece.__init__(self, colour)
 
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Return true iff attempted_location is diagonal on the board
-        to current_location. Return False otherwise."""
-
-        change_x = int(sqrt(current_location[0] - attempted_location[0])**2)
-        change_y = int(sqrt(current_location[1] - attempted_location[1])**2)
-
-        if change_x == change_y:
-            return True
-        return False
-
 
 class Queen(Piece):
     """A chess queen located on the chess board.
@@ -171,26 +91,6 @@ class Queen(Piece):
     def __init__(self, colour: str) -> None:
         Piece.__init__(self, colour)
 
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Return true iff attempted_location is diagonal on the board
-        or one space in any direction from current_location.
-        Return false otherwise."""
-
-        change_x = int(sqrt(current_location[0] - attempted_location[0])**2)
-        change_y = int(sqrt(current_location[1] - attempted_location[1])**2)
-
-        if change_x == change_y:
-            return True
-        elif current_location[0] == attempted_location[0] and \
-                (current_location[1] + 1 == attempted_location[1] or
-                 current_location[1] - 1 == attempted_location[1]):
-            return True
-        elif current_location[1] == attempted_location[1] and \
-                (current_location[0] + 1 == attempted_location[0] or
-                 current_location[0] - 1 == attempted_location[0]):
-            return False
-
 
 class King(Piece):
     """A chess king located on the chess board.
@@ -204,14 +104,3 @@ class King(Piece):
 
     def __init__(self, colour: str) -> None:
         Piece.__init__(self, colour)
-
-    def check_valid_move(self, current_location: tuple,
-                         attempted_location: tuple) -> bool:
-        """Return true iff attempted_location is one space away from
-        current_location in any direction."""
-        if current_location[0] - 1 <= \
-                attempted_location[0] <= current_location[0] + 1 and \
-                current_location[1] - 1 <= \
-                attempted_location[1] <= current_location[1] + 1:
-            return True
-        return False
